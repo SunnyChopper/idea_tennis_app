748c7a30cbcd3984c0b965b51e2c06fe52a17d91
Sunny Singh <ishy.singh@gmail.com>
2019-09-07 00:15:43 -0500

    Updated navbar and footer for name

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
-				<p class="text-center mb-0"><small>Copyright 2019 &copy; Tennis App</small></p>
+				<p class="text-center mb-0"><small>Copyright 2019 &copy; Tennis Helper App</small></p>
 			</div>
 		</div>
 	</div>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light">
 	<div class="container">
-		<a class="navbar-brand" href="#">Tennis App Idea</a>
+		<a class="navbar-brand" href="#">Tennis Helper App</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
